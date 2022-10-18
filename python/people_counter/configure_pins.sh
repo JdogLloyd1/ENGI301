6a4360441bbc5afda0b1111046f095df3c4d4dcf
Jonathan Lloyd <113152401+JdogLloyd1@users.noreply.github.com>
2022-10-18 17:17:00 -0500

    Update configure_pins.sh

--- python/people_counter/configure_pins.sh
+++ python/people_counter/configure_pins.sh
@@ -3,7 +3,7 @@
 # People Counter - Configure Pins
 # --------------------------------------------------------------------------
 # License:   
-# Copyright 2020 <Name>
+# Copyright 2020 Jonathan Lloyd
 # 
 # Redistribution and use in source and binary forms, with or without 
 # modification, are permitted provided that the following conditions are met:
