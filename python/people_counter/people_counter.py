352f25c96754f954653cd46c09808f4ce4481b06
Jonathan Lloyd <113152401+JdogLloyd1@users.noreply.github.com>
2022-10-18 17:17:21 -0500

    Update people_counter.py

--- python/people_counter/people_counter.py
+++ python/people_counter/people_counter.py
@@ -3,7 +3,7 @@
 People Counter
 --------------------------------------------------------------------------
 License:   
-Copyright 2022 <Name>
+Copyright 2022 Jonathan Lloyd
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
